15cd177d2313b689e679b98a9a4e9d1d3d91f233
HrithikaMestha <hrithikamestha48@gmail.com>
2026-05-16 21:40:57 +0530

    Added a javaFX files to the main repo

---
+++ mistiloop_Crochet/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.mistiloop_crochet {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.mistiloop_crochet to javafx.fxml;
+    exports org.example.mistiloop_crochet;
+}
